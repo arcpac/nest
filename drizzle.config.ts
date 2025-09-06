1e431f8677835ec00d83ee89cb253ef98dda5490
Antonio Caballes <antoniocaballes@Antonios-Mac-mini.local>
2025-09-06 22:04:47 +1000

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,6 @@ export default defineConfig({
   out: "./db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    // host: "localhost",
-    // port: 5432,
-    // user: "antoncaballes",
-    // password: "root",
-    // database: "nest",
     url: process.env.DATABASE_URL!, // your env variable
   },
 });
